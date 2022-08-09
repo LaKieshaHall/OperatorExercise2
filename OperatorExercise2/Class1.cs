15fd95fcc27469cf0ef0ba05cb8c70a2c44c5c7f
LaKiesha Hall <LaKiesha.Hall@Gmail.Com>
2022-08-09 18:12:56 -0400

    initial commit

---
+++ OperatorExercise2/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorExercise2
+{
+    internal class Class1
+    {
+        public static double AreaOfCircle(double radius)
+
+        {
+
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
+        }
+    }
+}
---
+++ OperatorExercise2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OperatorExercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+            var add = a + b;
+            var subtract = a - b;
+            var multiply = a * b;
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+            Console.WriteLine($"{a} / {b} is {quotient}");
+            Console.WriteLine($"{a} + {b} is {add}");
+            Console.WriteLine($"{a} - {b} is {subtract}");
+            Console.WriteLine($"{a} * {b} is {multiply}");
+
+            Console.WriteLine("What is the radius of the circle?");
+            var radius = double.Parse(Console.ReadLine());
+            Console.WriteLine(Class1.AreaOfCircle(radius));
+        }
+    }
+}
